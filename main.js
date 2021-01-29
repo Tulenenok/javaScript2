c27c4bb0f813e6eddda4099c8eaa025eaccbf739
Natasha Gurova <small-chinchilla@yandex.ru>
2021-01-29 19:29:45 +0300

    make oop for div products

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-const products = [
-    {id: 1, image: 'img/1.jpg', title: 'Яндекс.Книга', price: 1200},
-    {id: 2, image: 'img/2.jpg', title: 'Два капитана', price: 600},
-    {id: 3, image: 'img/3.jpg', title: 'Полдень 22 век', price: 750},
-    {id: 4, image: 'img/4.jpg', title: 'Физтеховская математика', price: 220}
-];
+class ProductItem {
+    constructor(product) {
+        this.id = product.id;
+        this.image = product.image;
+        this.title = product.title;
+        this.price = product.price;
+    }
 
-function makeProductHtml(product) {
-    return `<div class = 'product-item'>
-                <img class = 'products-image' src= ${product.image} alt="">
-                <p class = 'product-title'>${product.title}</p>
-                <p class = 'product-price'>${product.price}</p>
-                <button class = 'product-button'>Купить</button>
-            </div>`
-};
+    makeProductHtml() {
+        return `<div class = 'product-item'>
+                    <img class = 'products-image' src= ${this.image} alt="">
+                    <p class = 'product-title'>${this.title}</p>
+                    <p class = 'product-price'>${this.price}</p>
+                    <button class = 'product-button'>Купить</button>
+                </div>`
+    }
+}
 
-// Запятые были потому, что listOfHtml представлял из себя массив, в котором элементы были разделены запятыми
-// При добавлении этого массива целиком на страницу, запятые тоже добавлялись
-function InsertProductHtml(listOfProducts) {
-    const listOfHtml = listOfProducts.map(elem => makeProductHtml(elem));
-    listOfHtml.forEach(elem => document.querySelector('.products').insertAdjacentHTML('beforeend', elem));
-};
+class Products {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [
+            {id: 1, image: 'img/1.jpg', title: 'Яндекс.Книга', price: 1200},
+            {id: 2, image: 'img/2.jpg', title: 'Два капитана', price: 600},
+            {id: 3, image: 'img/3.jpg', title: 'Полдень 22 век', price: 750},
+            {id: 4, image: 'img/4.jpg', title: 'Физтеховская математика', price: 220}
+        ];
+    }
 
-InsertProductHtml(products);
+    InsertProductHtml() {
+        const block = document.querySelector(this.container);
+        this.goods.forEach(elem => {
+            const newProduct = new ProductItem(elem);
+            block.insertAdjacentHTML('beforeend', newProduct.makeProductHtml());
+        })
+    }
+}
+
+let list = new Products;
+list.InsertProductHtml();
+
+
